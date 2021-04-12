67666e9148b980e640b5ec89d7769d8534bc2a76
paschalidi <paschalidi@outlook.com>
2021-04-12 14:52:28 +0300

    chore: styles the tab navigation

--- src/App/AuthenticatedStack.js
+++ src/App/AuthenticatedStack.js
@@ -2,14 +2,37 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../domains/home/HomeScreen';
 import UserProfileScreen from '../domains/userProfile/UserProfileScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={UserProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={UserProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="user-astronaut"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
