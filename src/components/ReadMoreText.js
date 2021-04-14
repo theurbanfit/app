e79efef82a96cb628837a11fcb2fb9328a06aea8
paschalidi <paschalidi@outlook.com>
2021-04-14 16:53:58 +0300

    chore: edits readmore or less

--- src/components/ReadMoreText.js
+++ src/components/ReadMoreText.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-import {Button, Text} from 'react-native-paper';
+import {Text} from 'react-native-paper';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import {primary} from './colors';
 
 export const ReadMoreText = ({numberOfLines = 4, styles = {}, children}) => {
   const [expanded, toggleExpansion] = useState(false);
@@ -8,10 +10,21 @@ export const ReadMoreText = ({numberOfLines = 4, styles = {}, children}) => {
       <Text numberOfLines={expanded ? 0 : numberOfLines} style={styles}>
         {children}
       </Text>
-
-      <Button onPress={() => toggleExpansion(!expanded)}>
-        {expanded ? '-' : '+'} Read {expanded ? 'less' : 'more'}
-      </Button>
+      <TouchableOpacity
+        style={ownStyles.button}
+        onPress={() => toggleExpansion(!expanded)}>
+        <Text style={ownStyles.text}>
+          {expanded ? '-' : '+'} Read {expanded ? 'less' : 'more'}
+        </Text>
+      </TouchableOpacity>
     </>
   );
 };
+
+const ownStyles = StyleSheet.create({
+  button: {
+    marginTop: 12,
+    marginBottom: 12,
+  },
+  text: {color: primary, fontSize: 14},
+});
