981895e32d351425dea76058d3528e08d4ed4337
paschalidi <paschalidi@outlook.com>
2021-04-23 13:48:42 +0300

    chore: input is focused on open modal

--- src/components/SearchTopBar.js
+++ src/components/SearchTopBar.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {TouchableOpacity, Dimensions, StyleSheet, View} from 'react-native';
+import React, {useState, useEffect, createRef, useRef, forwardRef} from 'react';
+import {TouchableOpacity, StyleSheet, View} from 'react-native';
 import {
   Searchbar,
   Modal,
@@ -11,6 +11,8 @@ import {
 } from 'react-native-paper';
 import {divider, white} from './colors';
 import {Switch} from './Switch';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ContainerView} from './ContainerView';
 
 export const SearchTopBar = ({
   activeDistricts = [],
@@ -23,32 +25,28 @@ export const SearchTopBar = ({
   const hideModal = () => setVisible(false);
   const onChangeSearch = query => setSearchQuery(query);
 
-  console.log(visible);
   return (
     <>
       <TouchableOpacity onPress={() => showModal()}>
-        <View>
-          <Searchbar
-            style={styles.search}
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-          />
-        </View>
-        <SearchModal
-          onChangeSearch={onChangeSearch}
-          allowedDistricts={allowedDistricts}
-          visible={visible}
-          activeDistricts={activeDistricts}
-          onHideModal={hideModal}
-          onAllowedDistrictsSet={onAllowedDistrictsSet}
+        <Searchbar
+          style={[styles.search, styles.noShadow]}
+          inputStyle={{}}
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
         />
       </TouchableOpacity>
+      <SearchModal
+        onChangeSearch={onChangeSearch}
+        allowedDistricts={allowedDistricts}
+        visible={visible}
+        activeDistricts={activeDistricts}
+        onHideModal={hideModal}
+        onAllowedDistrictsSet={onAllowedDistrictsSet}
+      />
     </>
   );
 };
-
-let ScreenHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   search: {
@@ -58,22 +56,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  modalBackground: {
-    backgroundColor: white,
-  },
-  modalHeight: {
-    height: ScreenHeight,
-  },
-  marginFilters: {
-    marginBottom: 16,
-    marginTop: 20,
-  },
-  inline: {
-    marginBottom: 4,
-    marginTop: 4,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  noShadow: {
+    borderBottomColor: divider,
+    borderBottomWidth: 1,
+    shadowColor: 'transparent',
+    shadowOpacity: 0,
+    shadowRadius: 0, // elevation = 4
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
   },
 });
 
@@ -106,44 +98,90 @@ const SearchModal = ({
 
   return (
     <Portal>
-      <Modal
-        visible={visible}
-        onDismiss={onHideModal}
-        contentContainerStyle={styles.modalBackground}>
-        <View stlye={styles.modalHeight}>
-          <Searchbar
-            onIconPress={() => onHideModal()}
-            icon="arrow-left"
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-          />
-        </View>
-
-        <View style={styles.marginFilters}>
-          <Subheading>Select areas close to you</Subheading>
-          {activeDistricts?.map(({name, districtId}) => (
-            <View key={districtId} style={styles.inline}>
-              <Text>{name}</Text>
-              <Switch
-                isOn={allowedDistricts?.some(itemId => districtId === itemId)}
-                onValueChange={() => handleSwitchValueChange(districtId)}
-              />
-            </View>
-          ))}
-        </View>
-        <Dialog.Actions>
-          <Button uppercase={false} onPress={onHideModal}>
-            Cancel
-          </Button>
-          <Button
-            onPress={handleFilterAppliance}
-            uppercase={false}
-            style={{marginLeft: 12}}
-            mode="contained">
-            Apply filters
-          </Button>
-        </Dialog.Actions>
+      <Modal visible={visible} onDismiss={onHideModal}>
+        <SafeAreaView style={[modalStyles.container]}>
+          <View styles={[modalStyles.scrollView]}>
+            <Searchbar
+              ref={ref => ref?.focus()}
+              style={modalStyles.noShadow}
+              onIconPress={() => onHideModal()}
+              icon="arrow-left"
+              placeholder="Search"
+              onChangeText={onChangeSearch}
+            />
+            <ContainerView style={modalStyles.marginFilters}>
+              <Subheading>Select areas close to you</Subheading>
+              {activeDistricts?.map(({name, districtId}) => (
+                <View key={districtId} style={modalStyles.inline}>
+                  <Text>{name}</Text>
+                  <Switch
+                    isOn={allowedDistricts?.some(
+                      itemId => districtId === itemId,
+                    )}
+                    onValueChange={() => handleSwitchValueChange(districtId)}
+                  />
+                </View>
+              ))}
+            </ContainerView>
+          </View>
+          <View styles={[modalStyles.surface]}>
+            <Dialog.Actions>
+              <Button uppercase={false} onPress={onHideModal}>
+                Cancel
+              </Button>
+              <Button
+                onPress={handleFilterAppliance}
+                uppercase={false}
+                style={modalStyles.buttonMargin}
+                mode="contained">
+                Apply filters
+              </Button>
+            </Dialog.Actions>
+          </View>
+        </SafeAreaView>
       </Modal>
     </Portal>
   );
 };
+
+const modalStyles = StyleSheet.create({
+  container: {
+    height: '100%',
+    minHeight: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: white,
+  },
+  scrollView: {
+    flex: 1.9,
+  },
+  surface: {
+    alignSelf: 'flex-end',
+    flex: 0.1,
+  },
+  modalBackground: {},
+  noShadow: {
+    borderBottomColor: divider,
+    borderBottomWidth: 1,
+    shadowColor: 'transparent',
+    shadowOpacity: 0,
+    shadowRadius: 0, // elevation = 4
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+  },
+  marginFilters: {
+    marginBottom: 16,
+    marginTop: 20,
+  },
+  inline: {
+    marginBottom: 4,
+    marginTop: 4,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  buttonMargin: {marginLeft: 12},
+});
