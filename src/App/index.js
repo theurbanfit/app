6138a123e5eb649913648aa84e0ef50f105fa25e
paschalidi <paschalidi@outlook.com>
2021-04-24 18:52:32 +0300

    chore: text

--- src/App/index.js
+++ src/App/index.js
@@ -32,7 +32,7 @@ export default function Index() {
         <ProfileProvider>
           <Routes />
         </ProfileProvider>
-      </AuthProvider>y
+      </AuthProvider>
     </PaperProvider>
   );
 }
